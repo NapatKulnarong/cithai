eb3ba984ef5d82cb6ed6a22882f061245a996fad
NapatKulnarong <kul.napat@hotmail.com>
2026-03-23 14:51:00 +0700

    feat: register all domain models in Django Admin

--- songs/admin.py
+++ songs/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import User, MusicGenerationRequest, Song, ShareLink
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email']
+    search_fields = ['name', 'email']
+
+
+@admin.register(MusicGenerationRequest)
+class MusicGenerationRequestAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'user', 'mood', 'genre', 'submitted_at', 'is_retry']
+    list_filter  = ['mood', 'genre', 'occasion', 'is_retry']
+
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display  = ['id', 'title', 'user', 'status', 'is_shared', 'creation_date']
+    list_filter   = ['status', 'genre', 'mood', 'is_shared']
+    search_fields = ['title']
+
+
+@admin.register(ShareLink)
+class ShareLinkAdmin(admin.ModelAdmin):
+    list_display = ['id', 'song', 'token', 'is_active', 'created_at']
